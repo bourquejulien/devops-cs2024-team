33c9288fea051da6e996d3a85574951ce19fc467
Julien Bourque <julien@brqu.me>
2024-01-04 10:25:48 -0500

    Added terraform template

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "team_name" {
+    type = string
+    default = "00"
+    description = "team_name"
+}
