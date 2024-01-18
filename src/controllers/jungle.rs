0224773b96dd9bd6be803bb133e8b52299281cad
Julien Bourque <julien@brqu.me>
2024-01-18 12:00:32 -0500

    Exposing pod

--- src/controllers/jungle.rs
+++ src/controllers/jungle.rs
@@ -2,13 +2,17 @@ use axum::http::StatusCode;
 use axum::response::Html;
 
 pub async fn get_status() -> (StatusCode, Html<String>) {
-    let response = ureq::get("ai.private.dev.cs2024.one").call();
+    let response = ureq::get("http://ai.private.dev.cs2024.one/jungle").call();
 
-    if let Ok(data) = response {
-        if let Ok(str_data) = data.into_string() {
-            return (StatusCode::OK, Html(format!("<h1>Received: {str_data}<h1/>", )));
+    return match response {
+        Ok(data) => {
+            if let Ok(str_data) = data.into_string() {
+                return (StatusCode::OK, Html(format!("<h1>Received: {str_data}<h1/>", )));
+            }
+            (StatusCode::INTERNAL_SERVER_ERROR, Html("<h1>Call failed!!<h1/>".parse().unwrap()))
+        }
+        Err(err) => {
+            (StatusCode::INTERNAL_SERVER_ERROR, Html(format!("<h1>Call failed with reason: <h1/>\n{}", err)))
         }
     }
-
-    (StatusCode::INTERNAL_SERVER_ERROR, Html("<h1>Call failed<h1/>".parse().unwrap()))
 }
